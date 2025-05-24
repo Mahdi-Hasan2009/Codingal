3500b7dee1e24024c37e46662c114cb18859a162
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-05-24 15:16:55 +0600

    added activity 1 &2 for day 2

---
+++ Module_1/day_2/activity1.py
@@ -0,0 +1,17 @@
+a = input("Enter Side A: ")
+b = input("Enter Side B: ")
+c = input("Enter Side C: ")
+
+if a+b>c or b+c>a or a+c>b :
+  print("This is a Triangle")
+  if a == b == c:
+    print("It is Equilateral Triangle")
+  elif a == b or  b == c or c == a:
+    print("It is Isosceles Triangle")
+  else:
+    print("It is Scalene Tiangle")
+else:
+  print("Invalid Values of Triangle")
+ 
+
+
---
+++ Module_1/day_2/activity2.py
@@ -0,0 +1,4 @@
+base = float(input("Enter Base"))
+height = float(input("Enter Height"))
+area = 0.5*base*height
+print(f"Area = {area}")
