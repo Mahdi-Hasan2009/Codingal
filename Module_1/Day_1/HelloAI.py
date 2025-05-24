b8ac6ee4276f19a7d0c5b7e18892d4c781c0d679
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-05-24 12:59:09 +0600

    HEllo

--- Module_1/Day_1/HelloAI.py
+++ Module_1/Day_1/HelloAI.py
@@ -1,4 +1,4 @@
-# Greet the user 
+'''# Greet the user 
 print("Hello! I am AI Bot. What's your name? : ")
 
 # Get user input 
@@ -20,4 +20,47 @@ else:
   print("I see. Sometimes it's hard to put feelings into words.")
   
 # End the conversation
-print(f"It was nice chatting with you {name}. Goodbye!")
+print(f"It was nice chatting with you {name}. Goodbye!")'''
+'''
+num = int(input("Enter a number: "))
+
+if num%2 == 0:
+  print("Even")
+else:
+  print("Odd")
+  
+  
+def odd_or_even(num):
+  return "Even" if num ^1 == 0 else "Odd"'''
+
+
+principal = float(input("Enter Your Principal: "))
+rate = float(input("Enter Your Rate in percent: "))
+time = float(input("Enter Your Time: "))
+ratepercent = rate/100 
+Compound_Amount =principal * (1+ratepercent)**time
+Final_Amount = (principal*ratepercent*time)+principal
+interest = principal*ratepercent*time
+Compound_Interest = (principal * (1+ratepercent)**time)-principal
+print("Your Interest Is: ",interest) 
+print("Your Final Amount Is: ",Final_Amount)
+print("Your Compound Interest Is: ",Compound_Interest)
+print("Your Compound Amount Is: ",Compound_Amount)
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+
+
+
+
+
