faa45a693160092290e64fa5cfe64708235c9a46
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-05-24 16:07:02 +0600

    HW for class 2

---
+++ Module_1/day_2/HW.py
@@ -0,0 +1,9 @@
+year = int(input("Enter A Year: "))
+if year % 400 ==0:
+  print("It is a leapyear")
+else:
+  if year %4 == 0 and year % 100 != 0:
+    print("It is a leapyear")
+  else:
+    print("It is not a leapyear")
+  
