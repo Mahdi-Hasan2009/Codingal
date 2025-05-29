5dd4fc997e067ba7e3a8bd43c5475ad909c57c60
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-05-29 15:44:47 +0600

    activity factorial problem for class 3

--- Module_1/Day_3/factorial.py
+++ Module_1/Day_3/factorial.py
@@ -13,7 +13,6 @@ else:
   print(f"Factorial of {x} is {factorial} ")
   
   '''Problem:
-
 Write a program that prints numbers from 1 to 50. But for multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".'''
   
   
