b738543ea8e1be8cfb4c0dba08cc72850baf1a6f
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-05-29 16:39:12 +0600

    HW for day 3

--- Module_1/Day_3/HW.py
+++ Module_1/Day_3/HW.py
@@ -1,3 +1,14 @@
-word = input("Enter the word in lowercase: ")
+'''word = input("Enter the word in lowercase: ")
 upper = word.upper()
-print("The word in uppercase is:",upper)
+print("The word in uppercase is:",upper)'''
+
+ 
+for i in range(1,51):
+    if i % 3 == 0 and i % 5 == 0:
+       print("FizzBuzz")
+    elif i % 3 == 0:
+       print("Fizz")
+    elif i % 5 == 0:
+       print("Buzz")
+    else:
+       print(i)
