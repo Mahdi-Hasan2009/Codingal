12994fc6d9760eedb7fe24d7e1dcd63b2105f647
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-05-29 15:39:32 +0600

    activity factorial problem for class 3

---
+++ Day_3/factorial.py
@@ -0,0 +1,20 @@
+x= int(input("Enter a Number: "))
+
+factorial = 1
+
+
+if x<0 :
+  print("Please Give a Positive Number")
+elif x==0 :
+  print("1")
+else:
+  for i in range(1,x+1):
+    factorial*= i 
+  print(f"Factorial of {x} is {factorial} ")
+  
+  '''Problem:
+
+Write a program that prints numbers from 1 to 50. But for multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".'''
+  
+  
+  
--- Module_1/day_2/HW.py
+++ Module_1/day_2/HW.py
@@ -5,5 +5,5 @@ else:
   if year %4 == 0 and year % 100 != 0:
     print("It is a leapyear")
   else:
-    print("It is not a leapyear")
+    print("It is not a leapyear")    
   
