3e479a6ab908a793faceaf86a1a46d86bd32c37b
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-06-01 16:15:05 +0600

    HW for class 5

--- Module_1/Day_5/HW.py
+++ Module_1/Day_5/HW.py
@@ -1,1 +1,15 @@
 '''x must be even and lie between 50 and 100, but not divisible by 5.'''
+
+for i in range (50,101):
+  if i%2 == 0 and i%5 != 0 :
+    print(i)
+    
+'''
+temperature = float(input("Enter a Temperature: "))
+
+if temperature<20:
+  print("Wear Jacket")
+elif temperature>=20:     # *********  >=
+  print("Wear Light Cloth")
+
+'''
