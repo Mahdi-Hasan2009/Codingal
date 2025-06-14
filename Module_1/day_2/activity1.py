98d506d9f62fab80f64d5e72324f6b2bcaf5d926
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-06-14 15:14:39 +0600

    Activities for Day 8

--- Module_1/day_2/activity1.py
+++ Module_1/day_2/activity1.py
@@ -9,7 +9,7 @@ if a+b>c or b+c>a or a+c>b :
   elif a == b or  b == c or c == a:
     print("It is Isosceles Triangle")
   else:
-    print("It is Scalene Tiangle")
+    print("It is Scalene Triangle")
 else:
   print("Invalid Values of Triangle")
  
