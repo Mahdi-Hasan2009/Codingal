c832dc5916fb7e98fde9262611bb0cbf30907cba
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-06-21 16:07:24 +0600

    HW for class 10

---
+++ Module_2/Day_2/HW.py
@@ -0,0 +1,12 @@
+num=  int(input("Enter a number: "))
+power = int(input("Enter Power: "))
+result = 1
+for i in range(power):
+  result*=num
+print(result)
+'''
+num=  int(input("Enter a number: "))
+power = int(input("Enter Power: "))
+
+print(num**power)
+'''
