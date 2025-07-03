98c47f6746bf89a01416113492be4f99a4a71715
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-07-03 15:04:36 +0600

    HW for class 12

---
+++ Module_2/Day_4/HW.py
@@ -0,0 +1,13 @@
+number = int(input("Enter a number: "))
+binary = ""
+
+copy = number
+
+for i in range(number):
+    if copy == 0:
+        break
+    digit = copy % 2
+    binary = str(digit) + binary
+    copy = copy // 2
+
+print("Binary:", binary)
