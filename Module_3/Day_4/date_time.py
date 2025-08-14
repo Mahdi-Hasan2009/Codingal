7a72c4c7f350ed600e312e0ba9d14223ab43a7f9
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-08-14 15:49:46 +0600

    Activity for class 20

---
+++ Module_3/Day_4/date_time.py
@@ -0,0 +1,18 @@
+#import datetime
+import calendar
+
+yy = 2017
+mm =11
+
+print(calendar.month(yy,mm))
+
+from datetime import date,time,datetime
+
+today = date.today()
+now = datetime.now()
+print("Today's date is",today)
+print("\nCurrent Date and time is",now)
+
+print("\nDate components",today.year,today.month,today.day)
+
+
