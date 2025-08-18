c2cd9f75222e30c2a317fc5ea9e324a3f9802ac1
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-08-18 15:46:40 +0600

    HW for class 21

--- Module_4/Day_1/match.py
+++ Module_4/Day_1/match.py
@@ -1,8 +1,13 @@
-list1= [1,2,3,4,8]
-list2=[1,2,4,5]
-list3=[]
 
-for i in range(min(len(list1),len(list2))):
-  if list1[i] == list2[i]:
-    list3.append(list1[i])
+
+list1 = [1,2,3,4,8,9,10,1,1]
+list2 = [1,2,3,11,12,14,15,18,19,10,19,22,23,4,1]
+list3 = []
+
+for i in list1:
+    if i in list2 and i not in list3:
+        list3.append(i)
+
 print(list3)
+
+
