24dad56595343ddd5d2cadab5f755b518c635212
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-08-28 17:23:03 +0600

    HW for class 23

--- Module_4/Day_3/HW.py
+++ Module_4/Day_3/HW.py
@@ -1,21 +1,25 @@
 
-test_dict = input("Enter a tuple (like: (1, 2, 3)): ")
 
+
+
+
+
+test_dict = input("Enter a dictionary (like: {'Codingal': 3, 'is': 2, 'best': 2, 'for': 2, 'Coding': 1}): ")
 new_test_dict = eval(test_dict)
 
-while test_dict == "exit":
-  break 
 
-print("The original dictionary : " + str(test_dict))
+if test_dict == "exit":
+    exit()
 
 
-K = int(input("Enter a key: "))
+print("The original dictionary : " + str(new_test_dict))
+
+K = int(input("Enter a value to check frequency: "))
 
 res = 0
+for key in new_test_dict:
+    if new_test_dict[key] == K:
+        res = res +  1
 
-for key in test_dict:
-  if test_dict[key] == K:
-    res = res +1
-    
-    
-print("Frequency of K is : " + str(res))
+
+print("Frequency of", K, "is :", res)
