739cf9978784cb19196b8abd15edbb81538cc8d5
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-09-06 17:52:04 +0600

    Activity for class 27

---
+++ Module_4/Day_7/class_obj.py
@@ -0,0 +1,16 @@
+class car:
+  #brand mileage color
+  # obj 2 mercedes bmw
+  species = "car"
+  
+  def __init__(self,brand,mileage,color):
+    self.brand = brand
+    self.mileage = mileage
+    self.color = color
+    
+mercedes = car("mercedes",8786,"red")
+bmw = car("bmw",585,"blue")
+
+
+print(mercedes.brand , mercedes.color)
+print(bmw.mileage)
