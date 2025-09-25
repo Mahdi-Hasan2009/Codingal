dbba998ab0f0a6223b57e44d9c585a6192da9b82
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-09-25 20:24:49 +0600

    Activity

---
+++ Module_5/Day_4/Employeeinherit.py
@@ -0,0 +1,33 @@
+#parent class
+class Person(object):
+  
+  #__init__ known as constructor
+  
+  def __init__(self,name,id_number):
+    self.name = name
+    self.id_number = id_number
+    
+  def display(self):
+    print(self.name)
+    print(self.id_number)
+    
+    
+#child class
+class Employee(Person):
+  def __init__(self,name,id_number,salary,post):
+    super().__init__(name,id_number)
+    self.salary = salary
+    self.post = post
+    
+    
+# creation of an object variable or an instance
+
+a = Employee("Muhtadi",886012,200000,"Intern")
+
+# calling a function of the class Person using its instance
+
+a.display()
+
+
+
+    
