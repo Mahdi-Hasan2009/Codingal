eca94a94db256826fe4be658f4e7b1ddc79680ca
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2025-09-27 17:55:55 +0600

    CW

---
+++ Module_5/Day_5/cars.py
@@ -0,0 +1,31 @@
+from abc import ABC,abstractmethod
+
+class Car(ABC):
+  def __init__(self,wheels):
+    self.wheels = wheels
+  
+  def specialFeatures(self):
+    pass
+class Toyota(Car):
+  def __init__(self,wheels,color,mileage):
+    super().__init__(wheels)
+    self.color = color
+    self.mileage = mileage
+    
+  def specialFeatures(self):
+    print("Highest Mileage")
+
+class BMW(Car):
+  def __init__(self,wheels,color,mileage):
+    super().__init__(wheels)
+    self.color = color
+    self.mileage = mileage
+    
+  def specialFeatures(self):
+    print("Highest Mileage")
+
+  
+obj1 = Toyota(4,"white",100)
+print(obj1.color)
+obj1.specialFeatures()
+  
