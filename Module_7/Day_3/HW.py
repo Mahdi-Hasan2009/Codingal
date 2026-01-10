1f324fcf5d28043469d4ec022baa8b756fc9cc73
Muhtadi-Bin-Muhib <2010mbm5@gmail.com>
2026-01-10 14:39:16 +0600

    CW

--- Module_7/Day_3/HW.py
+++ Module_7/Day_3/HW.py
@@ -6,6 +6,22 @@ root = Tk()
 root.title("Length Converter App")    
 root.geometry("400x400")        
 
+Label(root,text= "write a length in inches").pack()
+entry1 = Entry(root)
+entry1.pack()
+
+def calculate():
+  
+  #entry2 = entry1
+  entry2 = entry1.get()
+  inches = float(entry2)
+  cm = inches*2.54
+  result_box.config(text= f'centimeter value {cm}')
+
+Button(root, text="Convert", command=calculate).pack(pady=10)
+
+result_box = Label(root,text = "")
+result_box.pack()
 
 
 root.mainloop()
